b5ba0f9708ebe1f01cab7337068affcf7bd83a1c
ShreenidhiHD <shreenidhihaagalagodu@gmail.com>
2024-10-10 20:59:05 +0530

    updated

--- tests/test_movements.py
+++ tests/test_movements.py
@@ -2,23 +2,23 @@ from unittest.mock import patch, MagicMock
 from selenium.webdriver.remote.webelement import WebElement
 from humani_py.movements import random_mouse_move
 
-@patch('humani_py.movements.ActionChains')
-@patch('humani_py.movements.random.uniform')
+@patch('selenium.webdriver.ActionChains')
+@patch('selenium.webdriver.remote.webdriver.WebDriver.execute_script')
 @patch('humani_py.movements.time.sleep')
-@patch('humani_py.movements.webdriver.WebDriver.execute_script')
-def test_random_mouse_move(mock_execute_script, mock_sleep, mock_uniform, mock_action_chains):
+@patch('humani_py.movements.random.uniform')
+def test_random_mouse_move(mock_uniform, mock_sleep, mock_execute_script, mock_action_chains):
     mock_driver = MagicMock()
     mock_element = MagicMock(spec=WebElement)
 
-    # Set mock return values for uniform and execute_script
-    mock_uniform.side_effect = [0, 0.5, 0.1]  # Mock overshoot and speed values
-    mock_execute_script.side_effect = [[500, 500]]  # Mock current mouse position
-    
+    # Mock return values
+    mock_uniform.side_effect = [0, 0.5, 0.1]
+    mock_execute_script.return_value = [500, 500]  # Mock the current mouse position
+
     # Create a mock ActionChains object
     mock_action_chain = mock_action_chains.return_value
 
     # Call the function being tested
-    random_mouse_move(mock_driver, mock_element, overshoot=True, easing=True, random_pauses=True, test_mode=True)
+    random_mouse_move(mock_driver, mock_element, test_mode=True)
 
     # Verify that ActionChains was instantiated with the driver
     mock_action_chains.assert_called_once_with(mock_driver)
@@ -32,7 +32,7 @@ def test_random_mouse_move(mock_execute_script, mock_sleep, mock_uniform, mock_a
     # Check that execute_script was called for getting mouse position
     mock_execute_script.assert_called()
 
-    # Check that random.uniform was used for random pauses and overshoot
+    # Check that random.uniform was used for overshoot and pauses
     mock_uniform.assert_any_call(0.1, 0.5)
 
     # Verify that sleep was called for pauses
